72e004b1b7604153977385b45b9204545cc60a8d
nghessss <voasd00@gmail.com>
2024-12-02 14:30:18 +0700

    abolish tone mark

--- fake_chatgpt_api/extract_gpt4o.py
+++ fake_chatgpt_api/extract_gpt4o.py
@@ -5,11 +5,11 @@ import csv
 import time
 
 # parent folder of the csv files
-csv_files = [f for f in os.listdir('./extracted') if f.endswith('.csv')]
+csv_files = [f for f in os.listdir('./for_trans') if f.endswith('.csv')]
 # merge all csv files into one
 merged_data = []
 for file in csv_files:
-    df = pd.read_csv(f'./extracted/{file}', header=None)
+    df = pd.read_csv(f'./for_trans/{file}', header=None)
     # remove first row
     df = df[1:]
     merged_data.append(df)
@@ -17,43 +17,76 @@ merged_data = pd.concat(merged_data)
 merged_data.dropna(inplace=True)
 modern_vietnamese_poems = merged_data[2].tolist()
 ancient_vietnamese_poems = merged_data[1].tolist()
-# drop the first row
-modern_vietnamese_poems = modern_vietnamese_poems[1:]
-ancient_vietnamese_poems = ancient_vietnamese_poems[1:]
+# # drop the first row
+# modern_vietnamese_poems = modern_vietnamese_poems[1:]
+# ancient_vietnamese_poems = ancient_vietnamese_poems[1:]
 
 def create_prompt(content):
     return (
         "Translate the following sentences from Vietnamese to English according to the requirements below\n"
-        "- Keep exactly the same number of lines in total (if input has 50 lines, output must have 50 lines too)\n"
-        "- Only print those lines, do not include any other output or explanation\n"
-        "- No diacritics, e.g. \"Lũng\" -> Lung\n"
-        "- Proper names should be written without diacritics or translated to their English equivalent, e.g. "
-        "\"Thượng Hải\" -> Shanghai, \"Anh Vũ\" -> Anh Vu, or \"Trương Hàn\" -> Truong Han\n"
-        f"{content}"
+        "    - Print each corresponding line (all without punctuation at the end)\n"
+        "    - Keep exactly number of lines in total (if input has 10 lines, output must have 10 lines too)\n"
+        "        - Example:\n"
+        "            + Input:\n"
+        "            1. Bãi thơm cây xanh biếc làm sao!\n"
+        "            2. Khói toả ra từ lá cây lan làm gió thơm nổi dậy\n"
+        "            3. Bờ liền với hoa đào, sóng gấm sinh\n"
+        "            + Output:\n"
+        "            1. The fragrant field the lush green trees how beautiful\n"
+        "            2. The smoke rising from the leaves of the trees carries the scent of the wind\n"
+        "            3. The shore merges with the peach blossoms and the brocade waves are born\n"
+        "    - Only print those lines, do not include any other output or explanation\n"
+        "    - No diacritics (\"Lũng\" -> Lung) and proper names should be written without diacritics or translated to their English equivalent (\"Thượng Hải\" -> Shanghai, \"Anh Vũ\" -> Anh Vu, or \"Trương Hàn\" -> Truong Han)\n"
+        "        - More example:\n"
+        "            + Input:\n"
+        "            1. Bãi trên sông mới truyền lại tên Anh Vũ\n"
+        "            2. Chim anh vũ đã bay về Tây qua núi Lũng\n"
+        "            + Output:\n"
+        "            1. The field on the river was where the name Anh Vu was passed down\n"
+        "            2. The Anh Vu bird has flown west over the Lung mountains\n"
+        f"\n{content}"
     )
 
+
+def not_match_line_prompt(content):
+    return (
+        "The previous output did not match the number of lines in the input.\n"
+        "Please translate those content again and make sure **the output must have exactly the same number of lines as the input**\n\n"
+        f"{content}"
+    )
+    
 # loop through each 50 lines each time
 batch_size = 50
 total_poem_sentences = 0
 
 fake = FakeChatGPTAPI()
-
+# clean csv file
 with open(f'translate.csv', 'w', newline='', encoding='utf-8') as csvfile:
     csvwriter = csv.writer(csvfile)
+    csvwriter.writerow(['vie', 'eng'])
+
 for i in range(0, len(modern_vietnamese_poems), batch_size):
+    print(i)
+    data = []
     modern_vietnamese_poems_batch = modern_vietnamese_poems[i:i+batch_size]
     ancient_vietnamese_poems_batch = ancient_vietnamese_poems[i:i+batch_size]
+    english = []
     text = ""
     for i in range(len(modern_vietnamese_poems_batch)):
         text += str(i) + ') ' + modern_vietnamese_poems_batch[i] + '\n'
-    # print(text)
-    data = []
-    respond_text = fake.send_request(text)
+
+    respond_text = fake.send_request(create_prompt(text))
     english_poem = respond_text.split('\n')
-    for i in range(len(english_poem)):
+
+    while (len(ancient_vietnamese_poems_batch) != len(english_poem)):
+        print('English translation does not match with Vietnamese texts')
+        time.sleep(10)
+        respond_text = fake.send_request(not_match_line_prompt(text))
+        english_poem = respond_text.split('\n')
+
+    for i in range(len(modern_vietnamese_poems_batch)):
         data.append([ancient_vietnamese_poems_batch[i], english_poem[i]])
-    # total_poem_sentences += 1
-    # write csv file with 2 columns vie,eng
+
     with open(f'translate.csv', 'a', newline='', encoding='utf-8') as csvfile:
         csvwriter = csv.writer(csvfile)
         # append the data to the csv file
